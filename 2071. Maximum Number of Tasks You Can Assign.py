1aeb8a0067cb35809720505ab13a8bea9f73f61f
Gokul <146116779+gokul-rm@users.noreply.github.com>
2025-05-01 22:26:26 +0530

    HARD

---
+++ 2071. Maximum Number of Tasks You Can Assign.py
@@ -0,0 +1,38 @@
+class Solution:
+    def maxTaskAssign(self, tasks, workers, pills, strength):
+        tasks.sort()
+        workers.sort()
+
+        def can_assign(mid):
+            boosted = deque()
+            w = len(workers) - 1
+            free_pills = pills
+
+            for i in range(mid - 1, -1, -1):
+                task = tasks[i]
+
+                if boosted and boosted[0] >= task:
+                    boosted.popleft()
+                elif w >= 0 and workers[w] >= task:
+                    w -= 1
+                else:
+                    while w >= 0 and workers[w] + strength >= task:
+                        boosted.append(workers[w])
+                        w -= 1
+                    if not boosted or free_pills == 0:
+                        return False
+                    boosted.pop()  
+                    free_pills -= 1
+            return True
+
+        low, high = 0, min(len(tasks), len(workers))
+        while low < high:
+            mid = (low + high + 1) // 2
+            if can_assign(mid):
+                low = mid
+            else:
+                high = mid - 1
+
+        return low
+
+        # O(n log n + m log m + k log k) time complexity
