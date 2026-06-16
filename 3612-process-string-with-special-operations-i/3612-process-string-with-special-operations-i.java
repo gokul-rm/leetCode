fdb8217d79ac0f4a449b8786bd93ff1934ffc7ac
Gokul <146116779+gokul-rm@users.noreply.github.com>
2026-06-16 08:52:26 +0530

    Time: 5 ms (20.59%), Space: 55.2 MB (64.71%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for(char ch : s.toCharArray()){
+            if(ch >= 'a' && ch<='z'){
+                sb.append(ch);
+            }
+            else if(ch == '*'){
+                if(sb.length() > 0)sb.setLength(sb.length() - 1);} 
+            else if(ch == '#') sb.append(sb);
+            else sb.reverse();
+        }
+        return sb.toString();
+    }
+}
