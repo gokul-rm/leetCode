5973e719c7de8fc2672eb38c45f6ce73ead79dc2
Gokul <146116779+gokul-rm@users.noreply.github.com>
2026-06-16 09:02:01 +0530

    Time: 227 ms (5.15%), Space: 77.4 MB (8.82%) - LeetHub

--- 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -1,16 +1,55 @@
+import java.util.*;
+
 class Solution {
     public String processStr(String s) {
-        StringBuilder sb = new StringBuilder();
+        Deque<Character> dq = new ArrayDeque<>();
+        boolean reversed = false;
 
-        for(char ch : s.toCharArray()){
-            if(ch == '%') sb.reverse();
-            else if(ch == '*'){
-                if(sb.length() > 0)sb.setLength(sb.length() - 1);} 
-            else if(ch == '#'){sb.ensureCapacity(sb.length() * 2);
-                sb.append(sb); }
-            else 
-            sb.append(ch);
+        for (char ch : s.toCharArray()) {
+            if (ch == '%') {
+                reversed = !reversed;
+            } 
+            else if (ch == '*') {
+                if (!dq.isEmpty()) {
+                    if (!reversed) {
+                        dq.removeLast();
+                    } else {
+                        dq.removeFirst();
+                    }
+                }
+            } 
+            else if (ch == '#') {
+                int size = dq.size();
+                Character[] arr = dq.toArray(new Character[0]);
+
+                if (!reversed) {
+                    for (int i = 0; i < size; i++) {
+                        dq.addLast(arr[i]);
+                    }
+                } else {
+                    for (int i = size - 1; i >= 0; i--) {
+                        dq.addFirst(arr[i]);
+                    }
+                }
+            } 
+            else {
+                if (!reversed) {
+                    dq.addLast(ch);
+                } else {
+                    dq.addFirst(ch);
+                }
+            }
         }
-        return sb.toString();
+
+        StringBuilder ans = new StringBuilder(dq.size());
+
+        if (!reversed) {
+            for (char c : dq) ans.append(c);
+        } else {
+            Iterator<Character> it = dq.descendingIterator();
+            while (it.hasNext()) ans.append(it.next());
+        }
+
+        return ans.toString();
     }
 }
