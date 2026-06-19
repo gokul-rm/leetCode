1ef6ebcaba0766cebc51be0829521b8b0f628947
Gokul <146116779+gokul-rm@users.noreply.github.com>
2026-06-19 16:16:43 +0530

    Time: 13 ms (99.95%), Space: 42.3 MB (16.29%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -3,7 +3,6 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int res = 0;
         int left = 1;
         int right = n;
 
@@ -11,13 +10,12 @@ public class Solution extends VersionControl {
             int mid  = left + (right - left) / 2;
 
             if(isBadVersion(mid)){
-                res = mid;
                 right = --mid;
             }
             else{
                 left = mid + 1;
             }
         }
-        return res;
+        return left;
     }
 }
