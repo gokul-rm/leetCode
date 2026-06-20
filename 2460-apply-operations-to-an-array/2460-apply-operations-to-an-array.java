ecd57ca39f1cffd11ed94ba0e38f7b866971666d
Gokul <146116779+gokul-rm@users.noreply.github.com>
2026-06-20 10:01:52 +0530

    Time: 1 ms (95.13%), Space: 44.7 MB (98.58%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.java
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.java
@@ -1,24 +1,22 @@
 class Solution {
     public int[] applyOperations(int[] nums) {
+        int count = 0;
         for(int i=1;i<nums.length;i++){
             if(nums[i-1] == nums[i]){
                 nums[i-1] = nums[i] * 2;
                 nums[i] = 0;
+                count++;
             }
         }
+        int idx = 0;
+        boolean passed = false;
         for(int i=0;i<nums.length;i++){
-            if(nums[i] == 0){
-                int k = i+1;
-                while(k < nums.length){
-                    if(nums[k] != 0){
-                    int temp = nums[k];
-                    nums[k] = nums[i];
-                    nums[i] = temp;
-                    break;
-                    }
-                    k++;
-                }
-            }
+            if(nums[i] != 0 && passed)  nums[idx++] = nums[i];
+            else if(nums[i] != 0 && !passed) idx++;
+            else passed = true;
+        }
+        for(int i=idx;i<nums.length;i++){
+            nums[idx++] = 0;
         }
         return nums;
     }
