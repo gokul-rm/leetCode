5b1cca4915666ee3769830a50a244a06767610bc
Gokul <146116779+gokul-rm@users.noreply.github.com>
2026-06-22 11:27:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int res = nums[0];
+        int minp = nums[0];
+        int maxp = nums[0];
+
+        for(int i=1;i<nums.length;i++){
+            if(nums[i] <=0){
+                int temp = maxp;
+                maxp = minp;
+                minp = temp;
+            }
+            minp = Math.max(nums[i],minp*nums[i]);
+            maxp = Math.max(nums[i],maxp*nums[i]);
+
+            res = Math.max(res,maxp);
+        }
+        return res;
+        
+    }
+}
