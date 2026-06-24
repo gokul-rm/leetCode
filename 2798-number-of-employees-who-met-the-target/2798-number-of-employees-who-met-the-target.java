8f2cd31162a990c5b4c76905ac46b596eaef4732
Gokul <146116779+gokul-rm@users.noreply.github.com>
2026-06-24 11:51:34 +0530

    Time: 0 ms (100%), Space: 44.3 MB (21.65%) - LeetHub

--- 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.java
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.java
@@ -1,16 +1,12 @@
 class Solution {
     public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
-
-        int count =0;
-
-        for(int i =0;i<hours.length;i++)
+        int n=hours.length;
+        int k=0;
+        for(int i=0;i<n;i++)
         {
             if(hours[i]>=target)
-            {
-                count++;
-            }
+            k++;
         }
-        return count;
-        
+        return k;
     }
 }
