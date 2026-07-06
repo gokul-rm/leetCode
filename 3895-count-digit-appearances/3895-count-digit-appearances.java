dd76143e94a647f4b619d1615304fadb045b11de
Gokul <146116779+gokul-rm@users.noreply.github.com>
2026-07-06 10:58:35 +0530

    Time: 4 ms (97.39%), Space: 46.9 MB (55.5%) - LeetHub

--- 3895-count-digit-appearances/3895-count-digit-appearances.java
+++ 3895-count-digit-appearances/3895-count-digit-appearances.java
@@ -1,14 +1,13 @@
 class Solution {
     public int countDigitOccurrences(int[] nums, int digit) {
-        int count = 0;
-        for (int n : nums) {
-            while (n > 0) {
-                if (n % 10 == digit) {
-                    count++;
-                }
-                n /= 10;
+        int[] arr = new int[10];
+
+        for(int num : nums){
+            while(num > 0){
+                arr[num%10]++;
+                num/=10;
             }
         }
-        return count;
+        return arr[digit];
     }
 }
