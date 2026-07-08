0fce51a2f4daae60d7231e1fc1fd4b493c5fe334
Gokul <146116779+gokul-rm@users.noreply.github.com>
2026-07-08 09:41:10 +0530

    Time: 13 ms (5.21%), Space: 46 MB (64.02%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -6,31 +6,26 @@ class Solution {
 
         if (s.length() == 0) return true;
 
-        if (map.containsKey(s)) {
-            return map.get(s);
-        }
+        if (map.containsKey(s)) return map.get(s);
 
         StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < s.length(); i++) {
-
             sb.append(s.charAt(i));
 
             if (wordDict.contains(sb.toString())) {
-
                 String remaining = s.substring(i + 1);
 
-                if (wordBreak(remaining, wordDict)) {
+                boolean found = wordBreak(remaining, wordDict);
 
+                if (found) {
                     map.put(s, true);
-
                     return true;
                 }
             }
         }
 
         map.put(s, false);
-
         return false;
     }
 }
